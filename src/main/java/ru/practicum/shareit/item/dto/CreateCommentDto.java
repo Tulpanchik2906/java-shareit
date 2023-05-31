47f60d9da08507d27c3ae45b68379a5560f8a137
Tulpanchik2906 <Shurpanchik2906@yandex.ru>
2023-05-31 14:38:07 +0300

    fix: убраны лишние поля из CreateCommentDto

--- src/main/java/ru/practicum/shareit/item/dto/CreateCommentDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/CreateCommentDto.java
@@ -1,21 +1,21 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
 
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateCommentDto {
-    private Long id;
     @NotNull
     @NotBlank
     private String text;
-    private String authorName;
-    private LocalDateTime created;
 
 }
